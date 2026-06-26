90f216e4f44c45bfd36f6caaf7e586713d5b2fc2
Rafat Khan <rafatkhan2210@gmail.com>
2026-06-26 11:38:12 +0600

    Add solution : inspection-for-glassell-coffee-shop.sql

---
+++ easy/inspection-for-glassell-coffee-shop.sql
@@ -0,0 +1,10 @@
+-- Problem: Inspection For Glassell Coffee Shop
+-- Difficulty: Easy
+-- Source: StrataScratch
+-- Link:https://platform.stratascratch.com/coding/9689-inspection-for-glassell-coffee-shop?code_type=1
+
+-- Solution:
+
+select * 
+from los_angeles_restaurant_health_inspections
+WHERE owner_name='GLASSELL COFFEE SHOP LLC'
